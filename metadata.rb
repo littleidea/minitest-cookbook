04a94f836985179389f7bbd3bfebc8e6e5c6be87
AJ Christensen <aj@junglist.gen.nz>
2011-07-22 17:18:21 -0700

    hotfix: add readme.rdoc & metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 maintainer       "AJ Christensen"
 maintainer_email "aj@junglist.gen.nz"
-license           "Apache 2.0"
+license          "Apache 2.0"
 description      "Installs minitest from gem and makes available some LWRP for intra-recipe usage."
 version          "0.0.1"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
